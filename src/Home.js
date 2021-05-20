eedcd0f90929e320162b28c06f22d989356582b2
Sreenidhi Bendre <67953001+sreenidhi3@users.noreply.github.com>
2021-05-20 21:26:44 +0530

    chnage: brand name

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ export const Home = ()=>{
         <div className="block home">
             <h1>Introduction</h1>
             <br/>
-            <h4>Lush UI is a simple, modular and accessible component library that helps you to rapidly develop applications with reusable building blocks.</h4>
+            <h4>Fleek UI is a simple, modular and accessible component library that helps you to rapidly develop applications with reusable building blocks.</h4>
             <br/> <br/>
             <p>To start using the components in your project import the stylesheet in your css or html file.</p>
             <br/><br/>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,11 @@
 import React  from 'react';
-import ".././lush-ui.png";
+import ".././fleek-ui.png";
 
 import { Link } from "react-router-dom";
 export const Sidebar=()=>{
     return(
         <ul className="sidebar">
-            <li className="nav-brand"><Link to="/"><img className="sidebar-logo"src="lush-ui.png" alt=""></img><h3>Lush UI</h3></Link></li>
+            <li className="nav-brand"><Link to="/"><img className="sidebar-logo"src="fleek-ui.png" alt=""></img><h3>Fleek UI</h3></Link></li>
             <li className ="sidebar-item"><Link  to="/">Home</Link></li>
             <li className ="sidebar-item"><Link to="/accordion">Accordion</Link></li>
             <li className ="sidebar-item"><Link to="/alert">Alert</Link></li>
@@ -17,7 +17,7 @@ export const Sidebar=()=>{
             <li className ="sidebar-item"><Link to="/input">Inputs</Link></li>
             <li className ="sidebar-item"><Link to="/list">Lists</Link></li>
             <li className ="sidebar-item"><Link to="/navbar">Navbar</Link></li>
-            <li><p>© Lush UI by Sreenidhi Bendre</p></li>
+            <li><p>© Fleek UI by Sreenidhi Bendre</p></li>
         </ul>
     )
 }
